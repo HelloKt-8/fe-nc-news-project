34bb1222864de1d3122e0b93b0bc6d9ec95352ab
Katie Luc <k_luc@hotmail.co.uk>
2024-06-20 12:25:56 +0100

    disables button when submitting comment

--- src/Individual Article Page/ArticlePage.jsx
+++ src/Individual Article Page/ArticlePage.jsx
@@ -8,9 +8,9 @@ import { getArticles, patchArticle } from "../API calls/getArticles";
 import { getComments, postNewComments } from "../API calls/getComments";
 
 const ArticlePage = () => {
-  const [article, setArticle] = useState([]);
+  const [article, setArticle] = useState({});
   const [comments, setComments] = useState([]);
-  const [votes, setVotes] = useState([]);
+  const [votes, setVotes] = useState(0);
   const [newComment, setNewComment] = useState("");
   const [commentStatus, setCommentStatus] = useState(false);
   const [isSubmitting, setIsSubmitting] = useState(false)
@@ -28,7 +28,6 @@ const ArticlePage = () => {
   }, [article_id]);
 
   const upVote = (article_id, voteValue) => {
-
     patchArticle(article_id, voteValue).then((data) => {
       setVotes(data.votes);
       setVotes((beforeVotes) => beforeVotes + voteValue);
@@ -36,11 +35,6 @@ const ArticlePage = () => {
         setVotes((beforeVotes) => beforeVotes - voteValue);
         console.log(err, "error patching like");
       });
-
-    setVotes((beforeVotes) => beforeVotes + voteValue);
-    patchArticle(article_id, voteValue).catch((err) => {
-      setVotes((beforeVotes) => beforeVotes - voteValue);
-      console.log(err, "error patching like");
     });
   };
 
@@ -78,7 +72,7 @@ const ArticlePage = () => {
   const handleClick = () => {
     setCommentStatus(false);
   };
-    
+
   return (
     <div>
       <div className="header-components">
@@ -103,7 +97,7 @@ const ArticlePage = () => {
         <img src={article.article_img_url} alt={article.topic} />
       </div>
       <div className="comments">
-      <form
+        <form
           className="CommentAdder"
           onSubmit={handleSubmit}
           id="commentAdder"
